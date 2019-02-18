2fdda4b8513ac930b99f336b2ca1fb49ab6a27b2
Ken <43943506+falconhour@users.noreply.github.com>
2019-02-18 16:16:15 +0800

    initialize for layout and functionality

--- src/ExercisePage.js
+++ src/ExercisePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Container, Col, Row, Alert } from 'react-bootstrap';
 import './ExercisePage.css';
 
 class ExercisePage extends Component {
@@ -10,44 +11,59 @@ class ExercisePage extends Component {
             lastName: null
         };
         this.handleOnChange = this.handleOnChange.bind(this);
+        this.handleOnSubmit = this.handleOnSubmit.bind(this);
     }
 
     handleOnChange(e) {
         this.setState({
             [e.target.name]: e.target.value
         });
+        this.jsonData.innerText = JSON.stringify(this.state);
+    }
+
+    handleOnSubmit(e) {
+        this.jsonData.innerText = JSON.stringify(this.state);
+        e.preventDefault();
     }
 
     render() {
         return (
             <div id='exercisePage'>
-                <form>
-                    <div className="form-group">
-                        <label>Email address</label>
-                        <br />
-                        <input type="email" className="form-control" 
-                            name='email'
-                            onChange={this.handleOnChange}/>
-                        <br />
-                    </div>
-                    <div className="form-group">
-                        <label>First Name</label>
-                        <br />
-                        <input type="email" className="form-control" 
-                            name='firstName'
-                            onChange={this.handleOnChange}/>
-                        <br />
-                    </div>
-                    <div className="form-group">
-                        <label>Last Name</label>
-                        <br />
-                        <input type="email" className="form-control"
-                            name='lastName'
-                            onChange={this.handleOnChange}/>
-                        <br />
-                    </div>
-                    <button>Submit</button>
-                </form>
+                <Container>
+                    <Row>
+                        <Col sm={6}>
+                            <form onSubmit={this.handleOnSubmit}>
+                                <div className="form-group">
+                                    <label>Email address</label>
+                                    <input className="form-control"
+                                        name='email'
+                                        onChange={this.handleOnChange} />
+                                </div>
+                                <div className="form-group">
+                                    <label>First Name</label>
+                                    <br />
+                                    <input className="form-control"
+                                        name='firstName'
+                                        onChange={this.handleOnChange} />
+                                </div>
+                                <div className="form-group">
+                                    <label>Last Name</label>
+                                    <input className="form-control"
+                                        name='lastName'
+                                        onChange={this.handleOnChange} />
+                                </div>
+                                <button>Submit</button>
+                            </form>
+                            <hr />
+                            <pre ref={(el) => {this.jsonData = el}}>JSON data goes here</pre>
+                        </Col>
+                        <Col sm={6}>
+                            <Alert variant='info'>
+                                Submitted data goes here!
+                            </Alert>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
