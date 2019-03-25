a697bdbe49af876111e8a22a950acb1fde923ac7
Ken <43943506+falconhour@users.noreply.github.com>
2019-03-25 15:28:59 +0800

    finished dynamic context
    finished dynamic context

--- src/Components/Context/DynamicContext.js
+++ src/Components/Context/DynamicContext.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 import OneSlicer from '../Slicers/OneSlicer';
 import { Container, Row, Col } from 'react-bootstrap';
+import './DynamicContext.css'
 
 const themes = {
-    navy: {
-        backgroundColor: '#3A435E'
-    },
     light: {
         backgroundColor: '#E9ECEF'
     },
@@ -14,23 +12,58 @@ const themes = {
     }
 }
 
-const MyContext = React.createContext(themes.light)
+const MyContext = React.createContext(themes.dark)
+
+class CustomButton extends Component {
+    render() {
+        let props = this.props;
+        let theme = this.context;
+        return (
+            <button {...props} style={theme} />
+        );
+    }
+}
+
+CustomButton.contextType = MyContext;
+
+const CustomButtonContainer = (props) => (
+    <div>
+        <CustomButton onClick={props.changeTheme} />
+    </div>
+);
 
 class DynamicContext extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { 
+            theme: themes.light
+        };
+        this.toggleTheme = () => {
+            this.setState(state => ({
+                theme:
+                    state.theme === themes.dark
+                        ? themes.light
+                        : themes.dark,
+            }));
+        }
     }
     render() {
         return (
             <OneSlicer>
-                <div className='module'>
+                <div className='module dynamic-context'>
                     <Container>
                         <Row>
                             <Col>
                                 <h1>Dynamic Context</h1>
                                 <div>
-                                    
+                                    <MyContext.Provider value={this.state.theme}>
+                                        <CustomButtonContainer changeTheme={this.toggleTheme} />
+                                    </MyContext.Provider>
+                                </div>
+                                <br />
+                                <br />
+                                <div>
+                                    <CustomButton />
                                 </div>
                             </Col>
                         </Row>
